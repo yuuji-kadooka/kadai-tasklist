20113f0007215fd361383b1fbec01175d88c3664
yuji kadooka <YUJIXSHIFON@YAHOO.CO.JP>
2018-09-16 06:21:25 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,29 @@
 
     <h1>作成ページ</h1>
     
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="col-sm-offset-2 col-sm-8">
+                <div class="col-md-offset-2 col-md-8">
+                    <div class="col-lg-offset-3 col-lg-6">
+                        {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+                            <div class="form-group">
+                                {!! Form::label('status', 'ステータス:') !!}
+                                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                            </div>
         
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+                            <div class="form-group">
+                                {!! Form::label('content', 'タスク:') !!}
+                                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                            </div>
         
-        {!! Form::submit('投稿') !!}
+                        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
-    {!! Form::close() !!}
-
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
