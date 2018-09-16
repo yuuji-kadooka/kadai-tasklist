5cdf993c7e83da06496fc044d6e444ac95e2b35a
yuji kadooka <YUJIXSHIFON@YAHOO.CO.JP>
2018-09-16 11:53:04 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,27 +5,21 @@
     <h1>id: {{ $task->id }} 編集ページ</h1>
     
     <div class="row">
-        <div class="col-xs-12">
-            <div class="col-sm-offset-2 col-sm-8">
-                <div class="col-md-offset-2 col-md-8">
-                    <div class="col-lg-offset-3 col-lg-6">
-                        {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-                            <div class="form-group">
-                                {!! Form::label('status', 'ステータス:') !!}
-                                {!! Form::text('status', null, ['class' => 'form-control']) !!}
-                            </div>
-                            
-                            <div class="form-group">
-                                {!! Form::label('content', 'タスク:') !!}
-                                {!! Form::text('content', null, ['class' => 'form-control']) !!}
-                            </div>
-        
-                            {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
-        
-                        {!! Form::close() !!}
-                    </div>
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
-            </div>
+                
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                    {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+        
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
