eb3b9522057551c4f5da645e927392df64217cfc
yuji kadooka <YUJIXSHIFON@YAHOO.CO.JP>
2018-09-17 15:05:31 +0000

    Procfile

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,7 +9,7 @@
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>タイトル</th>
+                    <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -17,7 +17,7 @@
                 @foreach ($tasks as $task)
                     <tr>
                         <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                        <td>{{ $task->title }}</td>
+                        <td>{{ $task->status }}</td>
                         <td>{{ $task->content }}</td>
                     </tr>
                 @endforeach
