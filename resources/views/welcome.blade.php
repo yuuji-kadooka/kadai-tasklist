053c709cf8c202ddc6db6fb438a3465d08946d96
yuji kadooka <YUJIXSHIFON@YAHOO.CO.JP>
2018-09-23 09:39:13 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+     @if (Auth::check())
+        <?php $user = Auth::user(); ?>
+        {{ $user->name }}
+    @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Tasklist</h1>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('tasks/{id}/copy', 'TasksController@copy');
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// ログイン認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
