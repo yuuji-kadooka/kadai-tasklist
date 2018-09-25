f6cb58013300ca09e8138c4c4629351c9ae2fd78
yuji kadooka <YUJIXSHIFON@YAHOO.CO.JP>
2018-09-25 03:04:50 +0000

    user pages

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,10 +10,19 @@ class UsersController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(10);
 
         return view('users.index', [
             'users' => $users,
+        ]);
+    }
+    
+    public function show($id)
+    {
+        $user = User::find($id);
+        
+        return view('users.show', [
+            'user' => $user,
         ]);
     }
 }
