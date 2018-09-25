75ace09a5ac2ef8b371431d484fab88a4c46d2e1
yuji kadooka <YUJIXSHIFON@YAHOO.CO.JP>
2018-09-25 02:21:37 +0000

    user login

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+}
