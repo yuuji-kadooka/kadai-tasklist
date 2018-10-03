4470d1d1262ef2a16732fc4ecdd1f861e0cbb130
yuji kadooka <YUJIXSHIFON@YAHOO.CO.JP>
2018-10-03 08:34:04 +0000

    tasks

--- database/migrations/2018_10_03_164614_create_user_id_table.php
+++ database/migrations/2018_10_03_164614_create_user_id_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusToTasksTable extends Migration
+class CreateUserIdTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,10 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status', 10);
+            $table->integer('user_id')->unsigned()->index();
+            
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -26,7 +29,7 @@ class AddStatusToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('status');
+            $table->dropColumn('user_id');
         });
     }
 }
